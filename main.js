cffe358d9d11e152bf43d1704a782bd2c451cf04
hoangngocvn21 <86026718+hoangngocvn21@users.noreply.github.com>
2021-06-17 06:19:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@
     b.type = "text/javascript";
 
     b.async = !0;
-    b.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    b.src = "https://raw.githubusercontent.com/hoangngocvn21/block-adblock/main/adsbygoogle.js";
     b.onerror = function() {
         f();
         window.adblock = !0
